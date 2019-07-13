c2c741f3a9a5c6555807ae0874c779fd71f4dcbd
J Karel Broer <jkarel@brovisuals.nl>
2019-07-13 15:21:23 +0200

    Installed initial Firebase helloWord function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    console.log('Hello from Firebase console log');
+    response.send("Hello from Firebase!");
+});
