acf16293f954e0a6202a8ec56506a81d8319a682
J Karel Broer <jkarel@brovisuals.nl>
2019-09-28 14:09:12 +0200

    iOS 13 update

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/karel/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/karel/Developer/Projects/unizer-mobile"
+export "FLUTTER_TARGET=/Users/karel/Developer/Projects/unizer-mobile/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/karel/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=4.0.0"
+export "FLUTTER_BUILD_NUMBER=9"
+export "TRACK_WIDGET_CREATION=true"
